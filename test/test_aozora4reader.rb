ba0a8f1abfbdca5724cebd3cdcbcc41daa650038
takahashim <maki@rubycolor.org>
2010-12-16 21:35:09 +0900

    Initial commit to aozora4reader.

---
+++ test/test_aozora4reader.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestAozora4reader < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
