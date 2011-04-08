9f71f1eb7885788497d413b157dc89836d045f83
liyuray <liyuray@gmail.com>
2011-04-08 15:11:59 +0800

    specify UTF-8 encoding when reading gaiji.txt and gaiji2.txt. If this is not specified, the test will fail on Windows Vista.

--- lib/aozora4reader.rb
+++ lib/aozora4reader.rb
@@ -326,7 +326,7 @@ END_OF_POST
   # 外字用ハッシュを作成
   def load_gaiji
     datadir = File.dirname(__FILE__)+"/../data"
-    File.open(datadir+"/gaiji.txt") do |f|
+    File.open(datadir+"/gaiji.txt", "r:UTF-8") do |f|
       while gaiji_line = f.gets
         gaiji_line.chomp!
         key, data = gaiji_line.split
@@ -334,7 +334,7 @@ END_OF_POST
       end
     end
 
-    File.open(datadir+"/gaiji2.txt") do |f|
+    File.open(datadir+"/gaiji2.txt", "r:UTF-8") do |f|
       while gaiji_line = f.gets
         gaiji_line.chomp!
         key, data = gaiji_line.split
